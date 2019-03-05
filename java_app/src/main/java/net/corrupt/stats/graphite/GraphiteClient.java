6ac5b66760ee3e8253c134fa14d7f2e9c6c51a2f
Dan McNevin <daniel.mcnevin@disney.com>
2019-03-04 20:11:17 -0800

    removed debugging

--- java_app/src/main/java/net/corrupt/stats/graphite/GraphiteClient.java
+++ java_app/src/main/java/net/corrupt/stats/graphite/GraphiteClient.java
@@ -28,7 +28,6 @@ public class GraphiteClient implements AutoCloseable {
         try (GraphiteClient client = new GraphiteClient(hostname, data)) {
             client.flattenVehicleData().forEach((s, o) -> {
                 String statsdData = String.format("tesla.%s:%s|%s", s, o, "g");
-                System.out.println("statsdData = " + statsdData);
                 try {
                     client.sendStat(statsdData);
                     LOGGER.log(Level.FINER, statsdData);
